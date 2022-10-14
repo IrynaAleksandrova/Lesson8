6228e3cb8a504dcf70a7fb606bf9f2b2f39fdf09
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-10-14 16:49:22 +0300

    all task

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    //    Класс Phone. Создайте класс Phone, который содержит переменные number, model и weight.
+//    Создайте три экземпляра этого класса. Выведите на консоль значения их переменных.
+//    Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит {name}”. getNumber –
+//    возвращает номер телефона. Вызвать эти методы для каждого из объектов.
+//    Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации переменных класса - number, model и weight.
+//    Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
+//    Добавить конструктор без параметров.
+//    Вызвать из конструктора с тремя параметрами конструктор с двумя.
+//    Добавьте перегруженный метод receiveCall, который принимает два параметра - имя звонящего и номер телефона звонящего. Вызвать этот метод.
+//    Создать метод sendMessage с аргументами переменной длины. Данный метод принимает на вход номера телефонов, которым будет отправлено
+//    сообщение. Метод выводит на консоль номера этих телефонов.
+    public static void main(String[] args) {
+
+        Phone iphone = new Phone(44123456, "IPhone", 15.74);
+        Phone xiaomi = new Phone(28456789, "Xiaomi", 17.23);
+        Phone vivo = new Phone(85789123, "Vivo", 17.81);
+
+        System.out.println(iphone);
+        System.out.println(xiaomi);
+        System.out.println(vivo);
+        System.out.println();
+
+        iphone.receiveCall("Olivia");
+        xiaomi.receiveCall("Mark");
+        vivo.receiveCall("Bob");
+        System.out.println();
+
+        System.out.println(iphone.getNumber());
+        System.out.println(xiaomi.getNumber());
+        System.out.println(vivo.getNumber());
+        System.out.println();
+
+        System.out.println(iphone.receiveCall("Olivia", iphone.getNumber()));
+        System.out.println(iphone.receiveCall("Mark", xiaomi.getNumber()));
+        System.out.println(iphone.receiveCall("Bob", vivo.getNumber()));
+        System.out.println();
+
+        iphone.sendMessage("Hello, what's up?", iphone.getNumber());
+        xiaomi.sendMessage(null, xiaomi.getNumber());
+        vivo.sendMessage("Hi, I'm here!" ,vivo.getNumber());
+    }
+}
---
+++ src/Phone.java
@@ -0,0 +1,46 @@
+public class Phone {
+    private int number;
+    private String model;
+    private double weight;
+
+    @Override
+    public String toString() {
+        return "Phone {" + "number = " + number + ", model = '" + model + '\'' + ", weight = " + weight + '}';
+    }
+
+    public Phone() {
+    }
+
+    public Phone(int number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(int number, String model, double weight) {
+        this(number, model);
+        this.weight = weight;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void receiveCall(String name) {
+        System.out.println(name + " is calling");
+    }
+
+    public String receiveCall(String name, int number) {
+        return name + " is calling... + " + number;
+    }
+
+    public void sendMessage(String mess, int... args) {
+        if (mess == null) {
+            System.out.println("Message text missing.");
+        } else {
+            System.out.println("Message text: " + mess);
+        }
+        for (int number : args) {
+            System.out.println("Number: " + number);
+        }
+    }
+}
